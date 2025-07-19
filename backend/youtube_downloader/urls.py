8275d955a8d297dba95f06050257420e33b650c5
Aeberl <duabellan@gmail.com>
2025-07-19 13:19:47 +0000

    Render deployment

--- backend/youtube_downloader/urls.py
+++ backend/youtube_downloader/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/', include('api.urls')),  # Your API endpoints
+    # Serve React's index.html for all other routes
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html'), name='home'),
 ]
-
